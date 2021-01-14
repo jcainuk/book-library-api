a6882086609cfb07b93be22d3abfe46642925737
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-01-14 14:11:33 +0000

    Routed POST create reader in app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const readerControllers = require('./controllers/readers');
 
 const app = express();
 app.use(express.json());
 
 module.exports = app;
 
+// test
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// readers
+
+app.post('/readers', readerControllers.create);
