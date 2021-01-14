a1ccff39bd42a5e75abe6d193bee5581e6410d0a
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-01-14 14:08:17 +0000

    Created the controllers folder and wrote first conteroller for create reader, importing Reader model

---
+++ src/controllers/readers.js
@@ -0,0 +1,17 @@
+const { Reader } = require('../models');
+
+/* Here we define the controller method like we used to do in the app.js file.
+ chaining  function calls.
+readerInfo = req.body;
+With the help of Sequelize, we create a new Reader record with the create() function.
+Assuming that the connection was declared it is equal to saying:
+connection.query("INSERT INTO artists VALUES (?, ?)", readerInfo.name, readerInfo.genre);
+Then, we go through the .then() if the creation worked.
+In .then() , we give back a response to the user, with a status 201
+ and with a body containing the data of the reader created
+ (because the .then() after the .create() provides us
+  with a parameter containing the created record). */
+
+exports.create = (req, res) => {
+  Reader.create(req.body).then((reader) => res.status(201).json(reader));
+};
