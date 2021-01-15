9e0be34c06b972583871265ccb519a86ede4e435
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-01-15 16:51:44 +0000

    Wrote the controller for DELETE readers

--- src/controllers/readers.js
+++ src/controllers/readers.js
@@ -5,7 +5,7 @@ const { Reader } = require('../models');
 readerInfo = req.body;
 With the help of Sequelize, we create a new Reader record with the create() function.
 Assuming that the connection was declared it is equal to saying:
-connection.query("INSERT INTO artists VALUES (?, ?)", readerInfo.name, readerInfo.genre);
+connection.query("INSERT INTO readers VALUES (?, ?)", readerInfo.name, readerInfo.genre);
 Then, we go through the .then() if the creation worked.
 In .then() , we give back a response to the user, with a status 201
  and with a body containing the data of the reader created
@@ -38,6 +38,17 @@ exports.updateReader = (req, res) => {
       res.status(404).json({ error: 'The reader could not be found.' });
     } else {
       res.status(200).json(rowsUpdated);
+    }
+  });
+};
+
+exports.deleteReader = (req, res) => {
+  const { id } = req.params;
+  Reader.destroy({ where: { id } }).then((rowsDeleted) => {
+    if (!rowsDeleted) {
+      res.status(404).json({ error: "The reader could not be found."});
+    } else {
+      res.status(204).json({ message: "Deleted successfully"});
     }
   });
 };
