f83f6d31cea774c9787a13ec2d64364f6213cf6c
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-01-17 16:07:58 +0000

    Wrote a controller for GET all book records

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -3,3 +3,7 @@ const { Book } = require('../models');
 exports.create = (req, res) => {
   Book.create(req.body).then((book) => res.status(201).json(book));
 };
+
+exports.list = (req, res) => {
+  Book.findAll().then((readers) => res.status(200).json(readers));
+};
