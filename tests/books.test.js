f3a58b5100d5f536b6403b35cd8b017fc181e130
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-01-17 15:00:51 +0000

    Wrote a describe for books with a before and BeforeEach

--- tests/books.test.js
+++ tests/books.test.js
@@ -2,3 +2,20 @@ const { expect } = require('chai');
 const request = require('supertest');
 const { Book } = require('../src/models');
 const app = require('../src/app');
+
+describe('/books', () => {
+  before(async () => {
+    try {
+      await Book.sequelize.sync();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  beforeEach(async () => {
+    try {
+      await Book.destroy({ where: {} });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+});
