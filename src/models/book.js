059c5d1a3b4e72ff92982894ed62b319a87b8a6c
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-01-18 13:16:34 +0000

    Changed  formatting of book model schema to allow for addition of more validation fields

--- src/models/book.js
+++ src/models/book.js
@@ -1,10 +1,17 @@
 module.exports = (connection, DataTypes) => {
   const schema = {
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    ISBN: DataTypes.STRING,
-
+    title: {
+      type: DataTypes.STRING,
+    },
+    author: {
+      type: DataTypes.STRING,
+    },
+    genre: {
+      type: DataTypes.STRING,
+    },
+    ISBN: {
+      type: DataTypes.STRING,
+    },
   };
   const BookModel = connection.define('Book', schema);
   return BookModel;
