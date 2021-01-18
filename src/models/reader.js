b576afe9bf63a75730e22a299a0c486a3e5d7373
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-01-18 19:36:07 +0000

    Deleted comments from reader model

--- src/models/reader.js
+++ src/models/reader.js
@@ -1,32 +1,3 @@
-// module.exports = (connection, DataTypes) => {
-//   const schema = {
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     email: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       validate: {
-//         isEmail: true,
-//       },
-//     },
-//     password: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       validate: {
-//         len: {
-//           args: [8, 100],
-//           msg: "Password must be at least 8 characters.",
-//         },
-//       },
-//     },
-//   };
-
-//   const readerModel = connection.define('reader', schema);
-//   return readerModel;
-// };
-
 module.exports = (connection, DataTypes) => {
   const schema = {
     name: {
