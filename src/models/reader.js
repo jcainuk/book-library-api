c27a9d976cedd13fa87900e672ab0c2e1f383758
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-01-18 13:19:20 +0000

    Changed reader model so null values not permitted

--- src/models/reader.js
+++ src/models/reader.js
@@ -2,12 +2,15 @@ module.exports = (connection, DataTypes) => {
   const schema = {
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
   };
   const ReaderModel = connection.define('Reader', schema);
