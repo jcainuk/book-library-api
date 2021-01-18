e09e135ca49d9ccd605fee9fdcafa10916a1a967
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-01-18 19:34:36 +0000

    Refactored reader model to include more validation messages

--- src/models/reader.js
+++ src/models/reader.js
@@ -1,13 +1,50 @@
+// module.exports = (connection, DataTypes) => {
+//   const schema = {
+//     name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     email: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: {
+//         isEmail: true,
+//       },
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: {
+//         len: {
+//           args: [8, 100],
+//           msg: "Password must be at least 8 characters.",
+//         },
+//       },
+//     },
+//   };
+
+//   const readerModel = connection.define('reader', schema);
+//   return readerModel;
+// };
+
 module.exports = (connection, DataTypes) => {
   const schema = {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter your name',
+        },
+      },
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
+        notNull: {
+          msg: 'Please enter your email',
+        },
         isEmail: true,
       },
     },
@@ -15,6 +52,9 @@ module.exports = (connection, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
+        notNull: {
+          msg: 'Please enter your password',
+        },
         len: {
           args: [8, 100],
           msg: "Password must be at least 8 characters.",
@@ -22,7 +62,6 @@ module.exports = (connection, DataTypes) => {
       },
     },
   };
-
-  const readerModel = connection.define('reader', schema);
+  const readerModel = connection.define('Reader', schema);
   return readerModel;
 };
