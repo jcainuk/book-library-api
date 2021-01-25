ab6fc4e43ad5f69f98eb4503dc2178b14fe9fa73
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-01-25 17:13:50 +0000

    created modules for authors and genres controllers

---
+++ src/controllers/authors.js
@@ -0,0 +1,2 @@
+const { Author } = require('../models');
+const { createItem, getAllItems, getItemById, updateItem, deleteItem, getAllBooks } = require('./helpers/helpers');
---
+++ src/controllers/genres.js
@@ -0,0 +1,2 @@
+const { Genre } = require('../models');
+const { createItem, getAllItems, getItemById, updateItem, deleteItem, getAllBooks } = require('./helpers/helpers');
