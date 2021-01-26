b84f18d73d1dd3e6b56b283f207546678903b91a
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-01-26 14:36:46 +0000

    Wrote the authors and genres controllers

--- src/controllers/authors.js
+++ src/controllers/authors.js
@@ -1,2 +1,22 @@
 const { Author } = require('../models');
 const { createItem, getAllItems, getItemById, updateItem, deleteItem, getAllBooks } = require('./helpers/helpers');
+
+exports.create = (req, res) => {
+  createItem(res, 'author', req.body);
+};
+
+exports.list = (req, res) => {
+  getAllItems(res, 'author');
+};
+
+exports.getAuthorById = (req, res) => {
+  getItemById(res, 'author', req.params.id);
+};
+
+exports.updateAuthor = (req, res) => {
+  updateItem(res, 'author', req.body, req.params.id);
+};
+
+exports.deleteAuthor = (req, res) => {
+  deleteItem(res, 'author', req.params.id);
+};
--- src/controllers/genres.js
+++ src/controllers/genres.js
@@ -1,2 +1,22 @@
 const { Genre } = require('../models');
 const { createItem, getAllItems, getItemById, updateItem, deleteItem, getAllBooks } = require('./helpers/helpers');
+
+exports.create = (req, res) => {
+  createItem(res, 'genre', req.body);
+};
+
+exports.list = (req, res) => {
+  getAllItems(res, 'genre');
+};
+
+exports.getGenreById = (req, res) => {
+  getItemById(res, 'genre', req.params.id);
+};
+
+exports.updateGenre = (req, res) => {
+  updateItem(res, 'genre', req.body, req.params.id);
+};
+
+exports.deleteGenre = (req, res) => {
+  deleteItem(res, 'genre', req.params.id);
+};
